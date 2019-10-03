8c2b12b63929646b986ede68727515fdc42518ad
Ninette <ninette.adhikari@gmail.com>
2019-10-03 17:45:11 +0200

    search filter added

--- client/src/components/ProductsList.js
+++ client/src/components/ProductsList.js
@@ -2,9 +2,22 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { Input } from "@material-ui/core";
+import { Carousel } from "react-responsive-carousel";
+import { TextField } from "@material-ui/core";
+
 export default class ProductsList extends Component {
   state = {
-    products: []
+    products: [],
+    search: ""
   };
 
   getData = () => {
@@ -25,31 +38,99 @@ export default class ProductsList extends Component {
     this.getData();
   };
 
+  handleSearch = event => {
+    const value = event.target.value;
+    console.log(value);
+    this.setState({
+      search: value
+    });
+  };
+
   render() {
+    const useStyles = makeStyles(theme => ({
+      card: {
+        maxWidth: 345
+      }
+    }));
+
+    const classes = useStyles;
+
+    const filteredProduct = this.state.products.filter(product => {
+      return (
+        product.name.toLowerCase().includes(this.state.search.toLowerCase()) ||
+        product.tags.find(tag => {
+          console.log("tag: ", tag, "search: ", this.state.search);
+          return Boolean(
+            tag.toLowerCase().includes(this.state.search.toLowerCase())
+          );
+        })
+      );
+    });
+
     return (
       <>
         <h1>Product List</h1>
-        <table>
-          <tr>
-            <th>Name</th>
-
-            <th>Description</th>
-            <th>Quantity</th>
-            <th>Price</th>
-          </tr>
-          {this.state.products.map(data => {
-            return (
-              <tr>
-                <Link to={`/products/${data._id}`}>
-                  <td>{data.name}</td>
-                </Link>
-                <td>{data.description}</td>
-                <td>{data.quantity}</td>
-                <td>{data.price}</td>
-              </tr>
-            );
-          })}
-        </table>
+        <TextField
+          label="search"
+          id="outlined-name-input"
+          type="search"
+          margin="normal"
+          className={classes.textField}
+          name="search"
+          variant="outlined"
+          autoComplete="search"
+          value={this.state.search}
+          onChange={this.handleSearch}
+        />
+        {filteredProduct.map(data => {
+          return (
+            <>
+              {/* <Carousel autoPlay>
+                {data.imageUrl.map(image => {
+                  console.log(image);
+                  return (
+                    <div>
+                      <img src={image} alt={image} />
+                    </div>
+                  );
+                })}
+              </Carousel> */}
+              <Card className={classes.card}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt={data.name}
+                    height="140"
+                    image={`${data.imageUrl[0]}`}
+                    title={data.name}
+                  />
+                  <CardContent>
+                    <Link to={`/products/${data._id}`}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {data.name}
+                      </Typography>
+                    </Link>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      {data.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Share
+                  </Button>
+                  <Button size="small" color="primary">
+                    Learn More
+                  </Button>
+                </CardActions>
+              </Card>
+            </>
+          );
+        })}
       </>
     );
   }
