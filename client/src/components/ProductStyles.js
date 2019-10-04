a33dbdde7883e3497c3bf897482e8160ff9fd41e
Ninette <ninette.adhikari@gmail.com>
2019-10-04 21:23:05 +0200

    material card styling

--- client/src/components/ProductStyles.js
+++
@@ -1,8 +0,0 @@
-const useStyles = theme => ({
-  card: {
-    maxWidth: 300,
-    margin: "0 auto"
-  }
-});
-
-export default useStyles;
--- client/src/components/ProductsList.js
+++ client/src/components/ProductsList.js
@@ -2,9 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import SearchFilter from "./SearchFilter";
-import useStyles from "./ProductStyles";
-
-import { Carousel } from "react-responsive-carousel";
+import useStyles from "./ProductListStyles";
 import {
   Typography,
   TextField,
