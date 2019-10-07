db76c972b974011a984f72e1edd026c52e3e5ab3
Noriko Takizawa <norikotakizawa@Norikos-MacBook-Pro.local>
2019-10-07 22:51:47 +0200

    add api to app.js

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ const index = require("./routes/index");
 app.use("/", index);
 
 const authRoutes = require("./routes/auth");
-app.use("/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 const productRoutes = require("./routes/productDetail");
 app.use("/api/products", productRoutes);
 
