cf0cfdb3c623b9a5189ae03201cd4e9086a8769f
Ninette <ninette.adhikari@gmail.com>
2019-10-08 15:56:06 +0200

    wip seller table

--- client/src/components/dashboard/DashboardSeller.js
+++ client/src/components/dashboard/DashboardSeller.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 import { Typography } from "@material-ui/core";
+import MaterialTable from "material-table";
 import { ProductContext } from "../../contexts/ProductContext";
 
 export default class DashboardSeller extends Component {
   static contextType = ProductContext;
+
+  state = {
+    columns: [
+      { title: "Title", field: "title" },
+      { title: "Quantity", field: "quantity" },
+      { title: "Price", field: "price" },
+      { title: "Sales status", field: "isSold" }
+    ],
+    data: this.context.state.products
+  };
 
   render() {
     console.log(this.context);
@@ -15,8 +26,40 @@ export default class DashboardSeller extends Component {
         id={`seller-tabpanel-${this.props.index}`}
         aria-labelledby="seller-tabpanel"
       >
-        helooss!!
-        {/* <Box p={3}>{children}</Box> */}
+        <MaterialTable
+          title="Items for Sale"
+          columns={this.state.columns}
+          data={this.state.data}
+          editable={{
+            // onRowAdd: newData =>
+            //   new Promise(resolve => {
+            //     setTimeout(() => {
+            //       resolve();
+            //       const data = [...this.state.data];
+            //       data.push(newData);
+            //       setState({ ...this.state, data });
+            //     }, 600);
+            //   }),
+            // onRowUpdate: (newData, oldData) =>
+            //   new Promise(resolve => {
+            //     setTimeout(() => {
+            //       resolve();
+            //       const data = [...state.data];
+            //       data[data.indexOf(oldData)] = newData;
+            //       setState({ ...state, data });
+            //     }, 600);
+            //   }),
+            onRowDelete: oldData =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  const data = [...this.state.data];
+                  data.splice(data.indexOf(oldData), 1);
+                  this.setState({ ...this.state, data });
+                }, 600);
+              })
+          }}
+        />
       </Typography>
     );
   }
