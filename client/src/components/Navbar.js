4fb4c3c7d2d5073ec37e13f6d1fd5eaaeaa326bc
Noriko Takizawa <noicomimo79@gmail.com>
2019-10-08 16:53:13 +0200

    add icon and style

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,10 +6,17 @@ import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
+import DashboardIcon from "@material-ui/icons/Dashboard";
 import ListItemText from "@material-ui/core/ListItemText";
 import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
+import StarsIcon from "@material-ui/icons/Stars";
+import MoodIcon from "@material-ui/icons/Mood";
+import FormatListNumberedIcon from "@material-ui/icons/FormatListNumbered";
+import HomeIcon from "@material-ui/icons/Home";
+import MeetingRoomIcon from "@material-ui/icons/MeetingRoom";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import MenuRoundedIcon from "@material-ui/icons/MenuRounded";
+import { blue, red } from "@material-ui/core/colors";
 import "../App.css";
 
 const useStyles = makeStyles({
@@ -48,29 +55,29 @@ export default function SwipeableTemporaryDrawer() {
     >
       <List>
         <ListItem>
-          <Link to="#">
-            <InboxIcon />
+          <Link to="#" style={{ color: "#616161", textDecoration: "none" }}>
+            <PermIdentityIcon />
             Profile
             <ListItemText />
           </Link>
         </ListItem>
         <ListItem>
-          <Link to="#">
-            <InboxIcon />
+          <Link to="#" style={{ color: "#616161", textDecoration: "none" }}>
+            <DashboardIcon />
             My Dashboard
             <ListItemText />
           </Link>
         </ListItem>
         <ListItem>
-          <Link to="#">
-            <InboxIcon />
+          <Link to="#" style={{ color: "#616161", textDecoration: "none" }}>
+            <StarsIcon />
             Wishlist
             <ListItemText />
           </Link>
         </ListItem>
         <ListItem>
-          <Link to="#">
-            <InboxIcon />
+          <Link to="#" style={{ color: "#616161", textDecoration: "none" }}>
+            <MoodIcon />
             Find Products
             <ListItemText />
           </Link>
@@ -79,15 +86,15 @@ export default function SwipeableTemporaryDrawer() {
       <Divider />
       <List>
         <ListItem>
-          <Link to="#">
-            <InboxIcon />
+          <Link to="#" style={{ color: "#616161", textDecoration: "none" }}>
+            <HomeIcon />
             Home Page
             <ListItemText />
           </Link>
         </ListItem>
         <ListItem>
-          <Link to="#">
-            <InboxIcon />
+          <Link to="#" style={{ color: "#616161", textDecoration: "none" }}>
+            <MeetingRoomIcon />
             Logout
             <ListItemText />
           </Link>
@@ -97,14 +104,9 @@ export default function SwipeableTemporaryDrawer() {
   );
 
   return (
-    <div>
+    <div classname="naVbar" style={{ textAlign: "right", position: "sticky" }}>
       <Button onClick={toggleDrawer("right", true)}>
-        Open Right{" "}
-        <div id="nav-icon">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
+        <MenuRoundedIcon fontSize="large" color="primary" />
       </Button>
       <SwipeableDrawer
         anchor="right"
