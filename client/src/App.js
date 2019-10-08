958568c767fe16c344f4bc837046e7eeefc00213
Noriko Takizawa <norikotakizawa@Norikos-MacBook-Pro.local>
2019-10-08 12:28:13 +0200

    remove all contact

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import ProductsList from "./components/products/ProductsList";
 
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
-import Contact from "./components/Contact";
 
 import ProductDetails from "./components/products/ProductDetails";
 import AddProduct from "./components/products/AddProduct";
@@ -16,22 +15,11 @@ export default class App extends Component {
     return (
       <div className="App">
         <Route exact path="/products" component={ProductsList} />
-
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/login" component={Login} />
-        <Route exact path="/contact" component={Contact} />
 
-        <Route
-          exact
-          path="/products/:id"
-          component={ProductDetails}
-        />
-        <Route
-          exact
-          path="/products/new"
-          component={AddProduct}
-        />
-
+        <Route exact path="/products/:id" component={ProductDetails} />
+        <Route exact path="/products/new" component={AddProduct} />
       </div>
     );
   }
